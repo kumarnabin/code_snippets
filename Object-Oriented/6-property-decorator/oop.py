48a49c6ea1a02e67eea59568d84083bf5ce166c1
Nabin Kumar Singh <kumarnabin@users.noreply.github.com>
2018-01-19 11:39:48 +0545

    Update oop.py

--- Object-Oriented/6-property-decorator/oop.py
+++ Object-Oriented/6-property-decorator/oop.py
@@ -12,9 +12,18 @@ class Employee:
     @property
     def fullname(self):
         return '{} {}'.format(self.first, self.last)
+    @fullname.setter
+    def fullname(self,value):
+        self.first,self.last=value.split(' ')
 
 
 emp_1 = Employee('John', 'Smith')
+
+print(emp_1.first)
+print(emp_1.email)
+print(emp_1.fullname)
+
+emp1.fullname="Nabin Singh"
 
 print(emp_1.first)
 print(emp_1.email)
